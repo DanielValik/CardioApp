e45f26c9a9109bf3735608163c4114cba42a92a3
DanielValik <danilvalik@gmail.com>
2023-11-04 15:07:40 +0100

    Add Workout

--- script.js
+++ script.js
@@ -8,6 +8,42 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputTemp = document.querySelector(".form__input--temp");
 const inputClimb = document.querySelector(".form__input--climb");
 
+const workouts = [];
+let type;
+
+class Workout {
+  id = (Date.now() + "").slice(-10);
+  date = new Date();
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; //km
+    this.duration = duration; //min
+  }
+
+  _setDescription() {
+    if(this.type === "running") this.description = 
+  }
+}
+
+class Running extends Workout {
+  type = "running";
+  constructor(coords, distance, duration, temp) {
+    super(coords, distance, duration);
+    this.temp = temp;
+    this.pace = this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  type = "cycling";
+  constructor(coords, distance, duration, climb) {
+    super(coords, distance, duration);
+    this.climb = climb;
+    this.speed = this.distance / this.duration;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
@@ -60,28 +96,103 @@ class App {
   _newWorkout(e) {
     e.preventDefault();
 
-    L.marker([this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng])
+    let workout;
+
+    const areNumbers = (...numbers) =>
+      numbers.every((elem) => Number.isFinite(elem));
+
+    const areNumbersPositive = (...numbers) =>
+      numbers.every((elem) => elem > 0);
+
+    //Coordinates from map event
+    const latitude = this.#mapEvent.latlng.lat;
+    const longitude = this.#mapEvent.latlng.lng;
+
+    //Get values from form
+    type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    //Check type of workout
+    if (type === "running") {
+      const temp = +inputTemp.value;
+
+      //Form validation
+      if (
+        !areNumbers(distance, duration, temp) ||
+        !areNumbersPositive(distance, duration, temp)
+      )
+        return alert("You type incorrect values1");
+
+      //Create Runing or Cycling obj
+      workout = new Running([latitude, longitude], distance, duration, temp);
+    }
+
+    if (type === "cycling") {
+      const climb = +inputClimb.value;
+
+      //Form validation
+      if (
+        !areNumbers(distance, duration, climb) ||
+        !areNumbersPositive(distance, duration)
+      )
+        return alert("You type incorrect values2");
+
+      //Create Runing or Cycling obj
+      workout = new Cycling([latitude, longitude], distance, duration, climb);
+    }
+
+    //Push new obj to workout array
+    workouts.push(workout);
+
+    //Display on the map
+    this._displayOnMap(workout);
+
+    //Display on the sidebar
+    this._displayOnSidebar(workout);
+
+    //Clear form
+    inputDistance.blur();
+    inputDuration.blur();
+    inputClimb.blur();
+    inputTemp.blur();
+    inputDistance.value =
+      inputDuration.value =
+      inputClimb.value =
+      inputTemp.value =
+        "";
+    form.classList.add("hidden");
+  }
+
+  _displayOnMap(workout) {
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
           content: "test",
           autoClose: false,
           closeOnClick: false,
-          className: "running-popup",
+          className: `${workout.type}-popup`,
         })
       )
       .openPopup();
+  }
 
-    inputCadence.blur();
-    inputDistance.blur();
-    inputDuration.blur();
-    inputClimb.blur();
-    inputCadence.value =
-      inputDistance.value =
-      inputDuration.value =
-      inputClimb.value =
-        "";
-    form.classList.add("hidden");
+  _displayOnSidebar(workout) {
+    html = `<li class="workout workout--${workout.type}" data-id="${
+      workout.id
+    }">
+    <h2 class="workout__title">${workout._setDescription()}</h2>
+    <div class="workout__details">
+      <span class="workout__icon">🏃</span>
+      <span class="workout__value">5.0</span>
+      <span class="workout__unit">km</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">⏱</span>
+      <span class="workout__value">21</span>
+      <span class="workout__unit">min</span>
+    </div>`;
   }
 }
 
